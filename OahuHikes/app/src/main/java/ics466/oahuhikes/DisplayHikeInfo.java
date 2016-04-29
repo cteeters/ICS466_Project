c61e5131bf6fcbc7eaef01d9afc3adc49978e3a4
Kenenji <kenenji71@gmail.com>
2016-04-28 15:55:40 -1000

    code for directions link

--- OahuHikes/app/src/main/java/ics466/oahuhikes/DisplayHikeInfo.java
+++ OahuHikes/app/src/main/java/ics466/oahuhikes/DisplayHikeInfo.java
@@ -6,10 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 
 public class DisplayHikeInfo extends AppCompatActivity
 {
-    private TextView textName, textLength, textDiff;
+    private TextView textName, textLength, textDiff, directions;
     private RatingBar ratingBar;
     SQLiteHelper myDB;
     Cursor results;
@@ -27,6 +29,7 @@ public class DisplayHikeInfo extends AppCompatActivity
         textLength = (TextView) findViewById(R.id.textLength);
         textDiff = (TextView) findViewById(R.id.textDiff);
         ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        directions = (TextView) findViewById(R.id.directions);
 
         Object extra = getIntent().getExtras().get("name");
         hikeName = extra.toString();
@@ -37,6 +40,12 @@ public class DisplayHikeInfo extends AppCompatActivity
         textLength.setText("Length: " + results.getString(1));
         textDiff.setText("Difficulty: " + results.getString(2));
         ratingBar.setRating(results.getFloat(3));
+
+//        Directions test
+        directions.setClickable(true);
+        directions.setMovementMethod(LinkMovementMethod.getInstance());
+        String text = "<a href='https://www.google.com/maps/place/Koko+Crater+Arch,+8483+HI-72,+Honolulu,+HI+96825/@21.2815724,-157.690732,15z/data=!3m1!4b1!4m2!3m1!1s0x7c001216253edcf7:0x9ef1880d4efab896'> Google Maps </a>";
+        directions.setText(Html.fromHtml(text));
     }
 
 }
