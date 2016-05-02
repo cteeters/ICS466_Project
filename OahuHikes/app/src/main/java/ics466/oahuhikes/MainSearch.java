979f1cff91cfcf7e18dac8ca9f85884259a2ed9e
cteeters <cteeters@hawaii.edu>
2016-05-02 10:10:42 -1000

    Submit on enter
    When searching, pressing the return key on the keyboard now submits the
    search.

--- OahuHikes/app/src/main/java/ics466/oahuhikes/MainSearch.java
+++ OahuHikes/app/src/main/java/ics466/oahuhikes/MainSearch.java
@@ -1,21 +1,26 @@
 package ics466.oahuhikes;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
@@ -41,6 +46,19 @@ public class MainSearch extends AppCompatActivity {
         myDB = new SQLiteHelper(this);
         editSearch = (EditText) findViewById(R.id.editText_Search);
         searchButton = (Button) findViewById(R.id.button_search);
+        editSearch.setOnEditorActionListener(new EditText.OnEditorActionListener()
+        {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event)
+            {
+                if(actionId == EditorInfo.IME_ACTION_DONE)
+                {
+                    searchButton.performClick();
+                    return true;
+                }
+                return false;
+            }
+        });
         radioButtonName = (RadioButton) findViewById(R.id.searchName);
         radioButtonLen = (RadioButton) findViewById(R.id.searchLength);
         radioButtonDiff = (RadioButton) findViewById(R.id.searchDiff);
--- OahuHikes/app/src/main/java/ics466/oahuhikes/SQLiteHelper.java
+++ OahuHikes/app/src/main/java/ics466/oahuhikes/SQLiteHelper.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
+import android.view.inputmethod.InputMethodManager;
 
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
